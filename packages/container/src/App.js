509f6afdd67f564c80b1f2820e2b4cc57c709909
Kevin Harders <kevin.harders@spreetail.com>
2021-07-16 08:18:55 -0500

    fix class collisions in prod

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+// helps keep classnames legitimately different - see notes on CSS Scoping
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container'
+})
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    // Ensures that generated styles are different between MFEs
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+// helps keep classnames legitimately different - see notes on CSS Scoping
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'marketing'
+})
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      {/* Ensures that generated styles are different between MFEs */}
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
